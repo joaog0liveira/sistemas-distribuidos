8287fa0a51e0b40e312085b9a86047365ba5ebe5
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2025-03-17 16:05:17 -0300

    Initial commit

---
+++ src/soquete/att1/Cliente.java
@@ -0,0 +1,32 @@
+package soquete.att1;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a região (ex: America/Sao_Paulo): ");
+        String region = scanner.nextLine();
+
+        byte[] sendData = region.getBytes();
+        InetAddress serverAddress = InetAddress.getByName("localhost");
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9876);
+        socket.send(sendPacket);
+
+        byte[] buffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(receivePacket);
+
+        String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Hora da região " + region + ": " + response);
+
+        socket.close();
+    }
+}
---
+++ src/soquete/att1/Servidor.java
@@ -0,0 +1,38 @@
+package soquete.att1;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Servidor {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(9876);
+        byte[] buffer = new byte[1024];
+
+        System.out.println("Servidor aguardando solicitações...");
+
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            String region = new String(packet.getData(), 0, packet.getLength()).trim();
+
+            String response;
+            try {
+                response = ZonedDateTime.now(ZoneId.of(region)).toString();
+            } catch (Exception e) {
+                response = "Região inválida!";
+            }
+
+            byte[] responseData = response.getBytes();
+            DatagramPacket responsePacket = new DatagramPacket(
+                    responseData, responseData.length, packet.getAddress(), packet.getPort()
+            );
+            socket.send(responsePacket);
+        }
+    }
+}
